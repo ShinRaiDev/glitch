bdd23e78c160c2504cb6bb44db8a095fdbe6ccc3
Shubh Chaudhary <shubhchaudhary@Shubhs-MacBook-Air.local>
2023-03-17 10:48:31 +0530

    dashboard done

--- app/dashboard/TotalTime.tsx
+++ app/dashboard/TotalTime.tsx
@@ -1,31 +1,57 @@
-"use client"
+"use client";
+import React from "react";
+import { BsInfinity } from "react-icons/bs"
 
-import React from 'react'
+function secondsToHms(time: any) {
+  let t = time;
+  var d = Math.floor(t/86400)
+  var h = Math.floor((t % 86400) / 3600);
+  var m = Math.floor(((t % 86400) % 3600) / 60);
+  var s = Math.floor(((t % 86400) % 3600) % 60);
 
-export default function TotalTime() {
+  var dDisplay = d > 0 ? d : "00";
+  var hDisplay = h > 0 ? h : "00";
+  var mDisplay = m > 0 ? m : "00";
+  var sDisplay = s > 0 ? s : "00";
+  let arr = [dDisplay,hDisplay, mDisplay, sDisplay];
+  return arr;
+}
+
+export default function TotalTime({ time }: any) {
+  const hms = secondsToHms(time);
+  console.log(hms);
   return (
-    <div className="grid auto-cols-max grid-flow-col gap-5 text-center">
+    <>
+      <div className="grid auto-cols-max grid-flow-col gap-5 text-center">
         <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
           <span className="countdown font-mono text-5xl">
             {/* @ts-ignore*/}
-            <span style={{ "--value": }}></span>
+            {hms[0]<=99? <span style={{ "--value": hms[0] }}> </span>:<BsInfinity/>}
+          </span>
+          days
+        </div>
+        <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
+          <span className="countdown font-mono text-5xl">
+            {/* @ts-ignore*/}
+            {hms[0]<=99? <span style={{ "--value": hms[1] }}> </span>:<BsInfinity/>}
           </span>
           hours
         </div>
         <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
           <span className="countdown font-mono text-5xl">
             {/* @ts-ignore*/}
-            <span style={{ "--value": }}></span>
+            {hms[0]<=99? <span style={{ "--value": hms[2] }}> </span>:<BsInfinity/>}
           </span>
           min
         </div>
         <div className="rounded-box flex flex-col bg-neutral p-2 text-neutral-content">
           <span className="countdown font-mono text-5xl">
             {/* @ts-ignore*/}
-            <span style={{ "--value": }}></span>
+            {hms[0]<=99? <span style={{ "--value": hms[3] }}> </span>:<BsInfinity/>}
           </span>
           sec
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
--- app/dashboard/card.tsx
+++ app/dashboard/card.tsx
@@ -1,21 +1,25 @@
-"use client"
+"use client";
 
-import { StudySession } from '@prisma/client'
-import React from 'react'
+import { StudySession } from "@prisma/client";
+import React from "react";
+import { secondsToHms } from "@/lib/utils/utils";
 
-export default function SessionCard({props}:any
-) {
+export default function SessionCard({ props }: any) {
+  const hms = secondsToHms(props.time);
   return (
     <div className="card w-96 bg-secondary text-base-100 shadow-xl">
-  
-  <div className="card-body">
-              <h2 className="card-title">{props.title}</h2>
-              <span>{props.description}</span>
-    <div className="card-actions justify-end flex">
-                  <span>Efficiency: {props.efficiency}</span>
-                  <span>Time: {props.time}</span>
+      <div className="card-body m-3 px-3 py-2">
+        <h2 className="card-title text-3xl font-bold">{props.title}</h2>
+        <span className="text-xl ">
+          {props.description}
+        </span>
+        <div className="card-actions m-3 flex flex-col items-end justify-end px-3 py-2 text-lg">
+          <span>Efficiency: {props.efficiency}</span>
+          <span>
+            Time Spent: {hms[0]}:{hms[1]}:{hms[2]}:{hms[3]}
+          </span>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
